5b5d2a17ee0651e120f03cd3801570cdd3df26f0
khagg <kellyahagg@gmail.com>
2024-06-03 23:50:39 -0700

    Top Interview 150 - Merge Sorted Array (88) O(n), O(1)

--- src/easy/MajorityElement.java
+++ src/easy/MajorityElement.java
@@ -2,10 +2,32 @@ package easy;
 
 import java.util.Arrays;
 
+//class MajorityElement {
+//    public int majorityElement(int[] nums) {
+//        Arrays.sort(nums);
+//        int n = nums.length;
+//        return nums[n/2];
+//    }
+//}
+
+// O(n) time complexity, O(1) space complexity
+// Boyer-Moore Voting Algorithm
 class MajorityElement {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return nums[n/2];
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+                count++;
+            } else if (candidate == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
     }
 }
